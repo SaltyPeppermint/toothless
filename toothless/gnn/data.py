5bc4efc7985d605baaa6fd563d9b04a80c9c381f
Nicole Heinimann <heinimann@tu-berlin.de>
2025-02-17 12:42:40 +0000

    Dataset now working, updated lock

--- toothless/gnn/data.py
+++ toothless/gnn/data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Self
+import json
 
 import torch
 import torch.nn.functional as F
@@ -9,7 +10,6 @@ from torch_geometric.data import InMemoryDataset
 
 from sklearn.model_selection import train_test_split
 import polars as pl
-import numpy as np
 
 from eggshell import rise  # type: ignore
 from toothless.utils import loading
@@ -46,24 +46,11 @@ def _pyrec_to_feature_vec(
 
 
 def _pick_indices(max_index: int, n: int) -> set[tuple[int, int]]:
-    rng = np.random.default_rng()
-    indices = rng.integers(low=0, high=max_index, size=n * 2)
+    indices = torch.randint(0, max_index, (n * 2,))
     first_half = indices[: len(indices) // 2]
     second_half = indices[len(indices) // 2 :]
 
     return set((int(x), int(y)) if x < y else (int(y), int(x)) for x, y in zip(first_half, second_half))
-
-
-def load_df(data_path) -> tuple[pl.DataFrame, pl.DataFrame]:
-    df = pl.read_parquet(data_path).select(["goal_expr", "middle_expr", "start_expr"])
-
-    # print(df.head())
-
-    test_size = 0.2
-    random_state = 42
-    train, eval = train_test_split(df, test_size=test_size, random_state=random_state)
-
-    return train, eval
 
 
 class PairDataset(InMemoryDataset):
@@ -74,6 +61,7 @@ class PairDataset(InMemoryDataset):
         var_names: list[str],
         ignore_unknown: bool,
         random_state: int = 0,
+        chunks: int = 50,
         transform=None,
         pre_transform=None,
         pre_filter=None,
@@ -84,11 +72,12 @@ class PairDataset(InMemoryDataset):
         self.var_names = var_names
         self.ignore_unknown = ignore_unknown
         self.random_state = random_state
+        self.chunks = chunks
 
+        self.len_memo = None
         self.cache_dir = Path("cache") / Path(*self.json_root.parts[2:])
 
         super().__init__(str(self.cache_dir), transform, pre_transform, pre_filter)
-        self.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
@@ -96,7 +85,7 @@ class PairDataset(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return ["processed_file_names.pt"]
+        return [f"data_{i}.pt" for i in range(self.chunks)] + ["meta.json"]
 
     def download(self):
         df = loading.load_df(self.json_root, self.var_names)
@@ -104,26 +93,78 @@ class PairDataset(InMemoryDataset):
 
     def process(self):
         # Read data into huge `Data` list.
+
         expl_chains = pl.read_parquet(self.raw_paths[0]).get_column("explanation_chain")
-        np.random.seed(self.random_state)
-        data_list = []
-        for chain in expl_chains:
-            index_pairs = _pick_indices(len(chain), self.pairs_per_expl)
-            selected_pair_data = [
-                PairData.from_tripple(
-                    rise.PyRecExpr(chain[a]), rise.PyRecExpr(chain[b]), a - b, self.var_names, self.ignore_unknown
-                )
-                for a, b in index_pairs
+        torch.manual_seed(self.random_state)
+
+        expl_chains = [rise.PyRecExpr.batch_new(i) for i in expl_chains]
+        print("Parallel processing done")
+
+        total_pairs = 0
+
+        picked_pairs = [_pick_indices(len(chain), self.pairs_per_expl) for chain in expl_chains]
+
+        total_pairs = sum([len(chain_pairs) for chain_pairs in picked_pairs])
+        print(f"Total pairs: {total_pairs}")
+        max_chunk_size = -(-total_pairs // self.chunks)  # Get number of full chunks -> round up
+        print(f"Chunk size: {max_chunk_size}")
+
+        save_buffer = []
+        spill_buffer = []
+        idx = 0
+
+        for chain, picked_pairs in zip(expl_chains, picked_pairs):
+            pairs = [
+                PairData.from_tripple(chain[a], chain[b], a - b, self.var_names, self.ignore_unknown)
+                for a, b in picked_pairs
             ]
-            data_list.extend(selected_pair_data)
 
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
+            if self.pre_filter is not None:
+                pairs = [data for data in pairs if self.pre_filter(data)]
 
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
+            if self.pre_transform is not None:
+                pairs = [self.pre_transform(data) for data in pairs]
 
-        self.save(data_list, self.processed_paths[0])
+            remaining_space = max_chunk_size - len(save_buffer)
+            save_buffer.extend(pairs[0:remaining_space])
+            spill_buffer.extend(pairs[remaining_space:])
+
+            if remaining_space == 0:
+                total_pairs += len(save_buffer)
+                print(f"Saving chunk {idx} in {self.processed_paths[idx]}")
+                self.save(save_buffer, self.processed_paths[idx])
+
+                save_buffer = spill_buffer
+                spill_buffer = []
+                idx += 1
+
+        # Save last few in the buffer if exist
+        if len(save_buffer) != 0:
+            total_pairs += len(save_buffer)
+            print(f"Saving rest in chunk {idx} in {self.processed_paths[idx]}")
+            self.save(save_buffer, self.processed_paths[idx])
+
+        print(f"Total lenght: {total_pairs}")
+        meta_path = Path(self.processed_dir) / Path("meta.json")
+        with open(meta_path, mode="w", encoding="utf-8") as f:
+            json.dump({"len": total_pairs, "max_chunk_size": max_chunk_size}, f)
+
+    def len(self):
+        if self.len_memo is None:
+            with open(Path(self.processed_dir) / Path("meta.json")) as f:
+                j = json.load(f)
+                self.len_memo = j["len"]
+                return j["len"]
+        else:
+            return self.len_memo
+
+    def get(self, idx):
+        max_chunk_size = -(-self.len() // self.chunks)  # Size of a full chunk
+        chunk = torch.load(Path(self.processed_paths[idx // max_chunk_size]))
+        return chunk[idx % max_chunk_size]
+
+    def to_dataloader(self, batch_size: int, shuffle: bool = False):
+        return DataLoader(data_list, follow_batch=["x_s", "x_t"], batch_size=batch_size, shuffle=shuffle)
 
 
 if __name__ == "__main__":
