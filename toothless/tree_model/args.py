ab743d0b660a091399799452917ad5c5bfe5c1d6
Nicole Heinimann <heinimann@tu-berlin.de>
2025-04-22 08:40:56 +0000

    bert-like parameter

--- toothless/tree_model/args.py
+++ toothless/tree_model/args.py
@@ -5,12 +5,12 @@ from typing import Optional
 @dataclass
 class ModelArguments:
     output_dir: str = field(default="model")
-    d_model: int = field(default=512, metadata={"help": "Hidden state dimension size."})
-    num_layers: int = field(default=8)
-    dim_feed_forward: int = field(default=2048, metadata={"help": "Feed forward dimension size."})
-    dropout: float = field(default=0.2)
-    anc_heads: int = field(default=6)
-    sib_heads: int = field(default=2)
+    d_model: int = field(default=768, metadata={"help": "Hidden state dimension size."})
+    num_layers: int = field(default=12)
+    dim_feed_forward: int = field(default=3072, metadata={"help": "Feed forward dimension size."})
+    dropout: float = field(default=0.1)
+    anc_heads: int = field(default=8)
+    sib_heads: int = field(default=4)
     pos_type: str = field(default="p2q_p2k", metadata={"help": "Position type seperated by underscore."})
     with_pos: bool = field(default=False, metadata={"help": "Enable rotary positional encoding for decoder."})
 
@@ -23,7 +23,7 @@ class DataArguments:
     min_expl_distance: int = field(
         default=8, metadata={"help": "Minimum distance when splitting the explanation chains recursively."}
     )
-    batch_size: int = field(default=64)
+    batch_size: int = field(default=16)
     k: int = field(default=15, metadata={"help": "Max relative positional distance."})
     max_len: int = field(default=128)
     force_reload: bool = field(default=False)
