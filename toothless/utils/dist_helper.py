2e1cc6b81051e7cfc23f1a434b8dd4a34381f058
Nicole Heinimann <heinimann@tu-berlin.de>
2025-05-06 13:14:41 +0000

    fix dist helper for single node training

--- toothless/utils/dist_helper.py
+++ toothless/utils/dist_helper.py
@@ -1,21 +1,21 @@
+import os
 import sys
 import torch.distributed as dist
-import logging
+
 
 def rank0print(rank, message):
-    if rank ==0:
+    if rank == 0:
         print(message)
 
+
 def rank0eprint(rank, message):
-    if rank ==0:
-        print(message,file=sys.stderr)
+    if rank == 0:
+        print(message, file=sys.stderr)
+
 
 def setup_process_group(rank, world_size):
-    if rank == 0:
-        logger = logging.getLogger(__name__)
-        logger.info("Setting up process group...")
-    # os.environ["MASTER_ADDR"] = "localhost"
-    # os.environ["MASTER_PORT"] = "12355"
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "6601"
 
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
