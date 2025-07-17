f7748bad479a8a9029b37e441c25835a7912af7d
Nicole Heinimann <heinimann@tu-berlin.de>
2025-07-17 11:01:48 +0000

    Cleaned up usefulness script

--- usefulness.py
+++ usefulness.py
@@ -9,12 +9,29 @@ from eggshell import rise  # type: ignore
 
 
 SAMPLES = 100
+TIME_LIMIT = 10.0
 
 
 @dataclass
 class CheckResult(JSONWizard):
     report: dict
     generation: int
+
+
+def check(
+    left: rise.RecExpr, right: rise.RecExpr, name: str, guide: rise.RecExpr | None = None
+) -> dict[str, CheckResult]:
+    report, generation, guide_used = rise.eqsat_check(
+        left,
+        right,
+        time_limit=TIME_LIMIT,
+        guides=[guide] if guide is not None else [],
+    )
+    tuple_reports = {name: CheckResult(json.loads(report), generation)}
+    if guide_used and guide is not None:
+        report, generation, _ = rise.eqsat_check(guide, right, time_limit=10.0)
+        tuple_reports[f"{name}_2"] = CheckResult(json.loads(report), generation)
+    return tuple_reports
 
 
 if __name__ == "__main__":
@@ -28,49 +45,38 @@ if __name__ == "__main__":
         left = rise.RecExpr(tuple["left"])
         right = rise.RecExpr(tuple["right"])
 
-        report, generation, _ = rise.eqsat_check(left, right, time_limit=10.0)
-        tuple_reports["baseline"] = CheckResult(json.loads(report), generation)
-
-        middle = rise.RecExpr(tuple["middle"])
-        report, generation, guide_used = rise.eqsat_check(left, right, time_limit=10.0, guides=[middle])
-        tuple_reports["middle"] = CheckResult(json.loads(report), generation)
-        if guide_used:
-            report, generation, _ = rise.eqsat_check(middle, right, time_limit=10.0)
-            tuple_reports["middle_2"] = CheckResult(json.loads(report), generation)
-
-        generated = rise.RecExpr(tuple["generated"])
-        report, generation, guide_used = rise.eqsat_check(left, right, time_limit=10.0, guides=[middle])
-        tuple_reports["generated"] = CheckResult(json.loads(report), generation)
-        if guide_used:
-            report, generation, _ = rise.eqsat_check(middle, right, time_limit=10.0)
-            tuple_reports["generated_2"] = CheckResult(json.loads(report), generation)
+        tuple_reports |= check(left, right, "baseline")
+        tuple_reports |= check(left, right, "middle", rise.RecExpr(tuple["middle"]))
+        tuple_reports |= check(left, right, "generated", rise.RecExpr(tuple["generated"]))
 
         reports.append(tuple_reports)
 
-    node_counts = {"baseline": 0, "middle": 0, "generated": 0}
+    max_node_counts = {"baseline": 0, "middle": 0, "generated": 0}
     stop_reasons = {"baseline": defaultdict(int), "middle": defaultdict(int), "generated": defaultdict(int)}
     reached_after_guide = {"middle": 0, "generated": 0}
 
     for report in reports:
-        node_counts["baseline"] += report["baseline"].report["egraph_nodes"]
+        max_node_counts["baseline"] += report["baseline"].report["egraph_nodes"]
         stop_reasons["baseline"][report["baseline"].report["stop_reason"]] += 1
 
-        node_counts["middle"] += report["middle"].report["egraph_nodes"]
+        max_node_counts["middle"] += report["middle"].report["egraph_nodes"]
         stop_reasons["middle"][report["middle"].report["stop_reason"]] += 1
 
         if "middle_2" in report:
-            node_counts["middle"] = max(node_counts["middle"], report["middle_2"].report["egraph_nodes"])
+            max_node_counts["middle"] = max(max_node_counts["middle"], report["middle_2"].report["egraph_nodes"])
             reached_after_guide["middle"] += 1
 
-        node_counts["generated"] += report["generated"].report["egraph_nodes"]
+        max_node_counts["generated"] += report["generated"].report["egraph_nodes"]
         stop_reasons["generated"][report["generated"].report["stop_reason"]] += 1
 
         if "generated_2" in report:
-            node_counts["generated"] = max(node_counts["generated"], report["generated_2"].report["egraph_nodes"])
+            max_node_counts["generated"] = max(
+                max_node_counts["generated"], report["generated_2"].report["egraph_nodes"]
+            )
             reached_after_guide["generated"] += 1
 
     with open("node_counts.json", mode="w", encoding="utf-8") as f:
-        json.dump(node_counts, f)
+        json.dump(max_node_counts, f)
 
     with open("stop_reasons.json", mode="w", encoding="utf-8") as f:
         json.dump(stop_reasons, f)
@@ -78,6 +84,9 @@ if __name__ == "__main__":
     with open("reached_after_guide.json", mode="w", encoding="utf-8") as f:
         json.dump(reached_after_guide, f)
 
-    print(node_counts)
+    print("---\nMAX NODE COUNT")
+    print(max_node_counts)
+    print("---\nSTOP REASONS")
     print(stop_reasons)
+    print("---\nREACHED AFTER GUIDE")
     print(reached_after_guide)
