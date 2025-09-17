886e547fbc27c48434393599a5166f133af10b8f
Nicole Heinimann <heinimann@tu-berlin.de>
2025-09-17 09:55:00 +0200

    adjust cache.py since tokenizer now part of dataset

--- cache.py
+++ cache.py
@@ -2,11 +2,9 @@ import tyro
 
 from toothless.data import TripleDataSet
 from toothless.args import DataArgs
-from toothless.tokenizer import build_tokenizer
 
 
 if __name__ == "__main__":
     data_args = tyro.cli(DataArgs)
     dataset = TripleDataSet(data_args)
-    build_tokenizer(dataset, data_args.tokenizer_samples)
     print(len(dataset))
