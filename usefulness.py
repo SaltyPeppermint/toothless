cf6eafe52c1a6e3a1eb951e9b668c572b8081705
Nicole Heinimann <heinimann@tu-berlin.de>
2025-09-22 16:33:56 +0200

    updated to sketchify the name in the guide, issue with overprecise goal persists

--- usefulness.py
+++ usefulness.py
@@ -2,6 +2,7 @@ import json
 import sys
 from pathlib import Path
 from collections import defaultdict
+from typing import DefaultDict
 import statistics
 
 from tqdm.auto import tqdm
@@ -11,45 +12,60 @@ from eggshell import rise  # type: ignore
 from toothless.inference import InferResult  # type: ignore
 
 
-MAX_SAMPLES = 50000
-TIME_LIMIT = 10.0
-BATCH_SIZE = 1
-WORKER_THREADS = 2
-
-MODEL_PATH = Path("25-08-05_13:25:52")
-
-
-def eqsat_check(left: rise.RecExpr, right: rise.RecExpr, name: str) -> dict[str, dict]:
-    report_str, _ = rise.eqsat_guide_check(left, right, time_limit=TIME_LIMIT, guides=[])
-    return {name: json.loads(report_str)}
-
-
-def eqsat_guide_check(left: rise.RecExpr, right: rise.RecExpr, guide: rise.RecExpr, name: str) -> dict[str, dict]:
-    report_str, guide_used = rise.eqsat_guide_check(
-        left, right, time_limit=TIME_LIMIT, guides=[guide] if guide is not None else []
-    )
-    report = {name: json.loads(report_str)}
-    if guide_used:
-        report_str, _ = rise.eqsat_guide_check(guide, right, time_limit=TIME_LIMIT)
-        report[f"{name}_2"] = json.loads(report_str)
-    return report
-
-
-def eqsat_rules_check(left: rise.RecExpr, right: rise.RecExpr, ordered_rules: list[str], name: str) -> dict[str, dict]:
-    ordered_rules = [r for r in ordered_rules if r]
-    report_str = rise.eqsat_ordered_rules_check(left, right, time_limit=TIME_LIMIT, ordered_rules=ordered_rules)
-    return {name: json.loads(report_str)}
+MAX_SAMPLES = 5000
+ITER_LIMIT = 6
 
 
 def check_tuple(sample: InferResult) -> dict[str, dict]:
     left = rise.RecExpr(sample.left)
     right = rise.RecExpr(sample.right)
 
-    tuple_report = eqsat_check(left, right, "baseline")
-    tuple_report |= eqsat_guide_check(left, right, rise.RecExpr(sample.middle), "middle")
-    tuple_report |= eqsat_guide_check(left, right, rise.RecExpr(sample.generated), "generated")
+    tuple_report = {}
+
+    report_str = rise.eqsat_check(left, right, iter_limit=ITER_LIMIT)
+    tuple_report["baseline"] = json.loads(report_str)["report"]
+
+    i_limit = min(ITER_LIMIT, tuple_report["baseline"]["iterations"] - 1)
+
+    middle_str = sample.middle.replace("[var]", "?")
+    report_str_1, report_str_2 = rise.eqsat_guide_check(left, right, rise.Guide(middle_str), iter_limit=i_limit)
+    tuple_report["middle"] = json.loads(report_str_1)["report"]
+    if report_str_2 is not None:
+        tuple_report["middle_2"] = json.loads(report_str_2)["report"]
+
+    report_str_1, report_str_2 = rise.eqsat_guide_check(left, right, rise.Guide(sample.generated), iter_limit=i_limit)
+    tuple_report["generated"] = json.loads(report_str_1)["report"]
+    if report_str_2 is not None:
+        tuple_report["generated_2"] = json.loads(report_str_2)["report"]
 
     return tuple_report
+
+
+def parse_guided_report(
+    report: dict,
+    name: str,
+    stop_reasons: dict[str, DefaultDict[str, int]],
+    guide_found: dict[str, int],
+    reached_after_guide: dict[str, int],
+    guide_reduced_mem: dict[str, int],
+    max_node: dict[str, int],
+):
+    max_node[name] = report[name]["egraph_nodes"]
+    if "Other" in report[name]["stop_reason"]:
+        sr = "Goal found"
+    else:
+        sr = str(list(report[name]["stop_reason"].keys())[0])
+    stop_reasons[name][sr] += 1
+
+    name_2 = f"{name}_2"
+    if name_2 in report:
+        guide_found[name] += 1
+        max_node[name] = max(max_node[name], report[name_2]["egraph_nodes"])
+        if "Goal found" in list(report[name_2]["stop_reason"].keys())[0]:
+            stop_reasons[name]["Goal Found"] += 1
+            reached_after_guide[name] += 1
+            if max_node[name] < max_node["baseline"]:
+                guide_reduced_mem[name] += 1
 
 
 if __name__ == "__main__":
@@ -78,46 +94,40 @@ if __name__ == "__main__":
             reports = json.load(f)
 
     max_nodes = []
-    stop_reasons = {"baseline": defaultdict(int), "middle": defaultdict(int), "generated": defaultdict(int)}
+    stop_reasons: dict[str, DefaultDict[str, int]] = {
+        "baseline": defaultdict(int),
+        "middle": defaultdict(int),
+        "generated": defaultdict(int),
+    }
+    guide_found = {"middle": 0, "generated": 0}
     reached_after_guide = {"middle": 0, "generated": 0}
     guide_reduced_mem = {"middle": 0, "generated": 0}
 
     for report in reports:
         max_node = {}
 
-        max_node["baseline"] = report["baseline"]["report"]["egraph_nodes"]
-        sr = list(report["baseline"]["report"]["stop_reason"])[0]
-        stop_reasons["baseline"][sr] += 1
-
-        max_node["middle"] = report["middle"]["report"]["egraph_nodes"]
-        sr = list(report["middle"]["report"]["stop_reason"])[0]
-        stop_reasons["middle"][sr] += 1
-
-        if "middle_2" in report:
-            max_node["middle"] = max(max_node["middle"], report["middle_2"]["report"]["egraph_nodes"])
-            reached_after_guide["middle"] += 1
-            if max_node["middle"] < max_node["baseline"]:
-                guide_reduced_mem["middle"] += 1
-
-        max_node["generated"] = report["generated"]["report"]["egraph_nodes"]
-        sr = list(report["generated"]["report"]["stop_reason"])[0]
-        stop_reasons["generated"][sr] += 1
-
-        if "generated_2" in report:
-            max_node["generated"] = max(max_node["generated"], report["generated_2"]["report"]["egraph_nodes"])
-            reached_after_guide["generated"] += 1
-            if max_node["generated"] < max_node["baseline"]:
-                guide_reduced_mem["generated"] += 1
+        parse_guided_report(
+            report, "baseline", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+        )
+        parse_guided_report(
+            report, "middle", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+        )
+        parse_guided_report(
+            report, "generated", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+        )
 
         max_nodes.append(max_node)
 
-    stop_reasons = {k: dict(v) for k, v in stop_reasons.items()}
+    stop_reasons_dict = {k: dict(v) for k, v in stop_reasons.items()}
 
     with open(usefulness_path / "node_counts.json", mode="w", encoding="utf-8") as f:
         json.dump(max_nodes, f)
 
     with open(usefulness_path / "stop_reasons.json", mode="w", encoding="utf-8") as f:
-        json.dump(stop_reasons, f)
+        json.dump(stop_reasons_dict, f)
+
+    with open(usefulness_path / "guide_found.json", mode="w", encoding="utf-8") as f:
+        json.dump(guide_found, f)
 
     with open(usefulness_path / "reached_after_guide.json", mode="w", encoding="utf-8") as f:
         json.dump(reached_after_guide, f)
@@ -132,6 +142,8 @@ if __name__ == "__main__":
 
     print("---\nSTOP REASONS")
     print(stop_reasons)
+    print("---\nGUIDES FOUND")
+    print(guide_found)
     print("---\nREACHED AFTER GUIDE")
     print(reached_after_guide)
     print("---\nGUIDE REDUCED MEM")
