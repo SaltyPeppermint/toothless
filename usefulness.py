ca8deeb054dfaa5b7f4eedf1eaa767deff84a010
Nicole Heinimann <heinimann@tu-berlin.de>
2025-09-23 10:19:28 +0200

    usefulness check if goal and guide are sketch

--- usefulness.py
+++ usefulness.py
@@ -12,7 +12,7 @@ from eggshell import rise  # type: ignore
 from toothless.inference import InferResult  # type: ignore
 
 
-MAX_SAMPLES = 5000
+MAX_SAMPLES = 5
 ITER_LIMIT = 6
 
 
@@ -20,23 +20,39 @@ def check_tuple(sample: InferResult) -> dict[str, dict]:
     left = rise.RecExpr(sample.left)
     right = rise.RecExpr(sample.right)
 
-    tuple_report = {}
+    tuple_report = defaultdict(dict)
 
-    report_str = rise.eqsat_check(left, right, iter_limit=ITER_LIMIT)
-    tuple_report["baseline"] = json.loads(report_str)["report"]
+    report_str, reached = rise.eqsat_check(left, right, iter_limit=ITER_LIMIT)
+    tuple_report["baseline"]["report"] = json.loads(report_str)["report"]
+    tuple_report["baseline"]["reached"] = reached
 
-    i_limit = min(ITER_LIMIT, tuple_report["baseline"]["iterations"] - 1)
+    i_limit = min(ITER_LIMIT, tuple_report["baseline"]["report"]["iterations"] - 1)
 
     middle_str = sample.middle.replace("[var]", "?")
-    report_str_1, report_str_2 = rise.eqsat_guide_check(left, right, rise.Guide(middle_str), iter_limit=i_limit)
-    tuple_report["middle"] = json.loads(report_str_1)["report"]
-    if report_str_2 is not None:
-        tuple_report["middle_2"] = json.loads(report_str_2)["report"]
+    report_str_1, report_str_2, reached = rise.eqsat_guide_check(
+        left, right, rise.Guide(middle_str), iter_limit=i_limit
+    )
+    tuple_report["middle"]["report"] = json.loads(report_str_1)["report"]
+    tuple_report["middle"]["reached"] = reached
+    if reached:
+        tuple_report["middle"]["report_2"] = json.loads(report_str_2)["report"]
 
-    report_str_1, report_str_2 = rise.eqsat_guide_check(left, right, rise.Guide(sample.generated), iter_limit=i_limit)
-    tuple_report["generated"] = json.loads(report_str_1)["report"]
-    if report_str_2 is not None:
-        tuple_report["generated_2"] = json.loads(report_str_2)["report"]
+    right_str = sample.right.replace("[var]", "?")
+    report_str_1, report_str_2, reached = rise.eqsat_two_guide_check(
+        left, rise.Guide(right_str), rise.Guide(middle_str), iter_limit=i_limit
+    )
+    tuple_report["middle_as_sketch"]["report"] = json.loads(report_str_1)["report"]
+    tuple_report["middle_as_sketch"]["reached"] = reached
+    if reached is not None:
+        tuple_report["middle_as_sketch"]["report_2"] = json.loads(report_str_2)["report"]
+
+    report_str_1, report_str_2, reached = rise.eqsat_guide_check(
+        left, right, rise.Guide(sample.generated), iter_limit=i_limit
+    )
+    tuple_report["generated"]["report"] = json.loads(report_str_1)["report"]
+    tuple_report["generated"]["reached"] = reached
+    if reached:
+        tuple_report["generated"]["report_2"] = json.loads(report_str_2)["report"]
 
     return tuple_report
 
@@ -45,27 +61,28 @@ def parse_guided_report(
     report: dict,
     name: str,
     stop_reasons: dict[str, DefaultDict[str, int]],
-    guide_found: dict[str, int],
-    reached_after_guide: dict[str, int],
-    guide_reduced_mem: dict[str, int],
+    goal_reached: DefaultDict[str, int],
+    guide_reached: DefaultDict[str, int],
+    reached_after_guide: DefaultDict[str, int],
+    guide_reduced_mem: DefaultDict[str, int],
     max_node: dict[str, int],
 ):
-    max_node[name] = report[name]["egraph_nodes"]
-    if "Other" in report[name]["stop_reason"]:
+    max_node[name] = report[name]["report"]["egraph_nodes"]
+    if "Other" in report[name]["report"]["stop_reason"]:
         sr = "Goal found"
     else:
-        sr = str(list(report[name]["stop_reason"].keys())[0])
+        sr = str(list(report[name]["report"]["stop_reason"].keys())[0])
     stop_reasons[name][sr] += 1
 
-    name_2 = f"{name}_2"
-    if name_2 in report:
-        guide_found[name] += 1
-        max_node[name] = max(max_node[name], report[name_2]["egraph_nodes"])
-        if "Goal found" in list(report[name_2]["stop_reason"].keys())[0]:
-            stop_reasons[name]["Goal Found"] += 1
-            reached_after_guide[name] += 1
-            if max_node[name] < max_node["baseline"]:
-                guide_reduced_mem[name] += 1
+    if report[name]["reached"]:
+        goal_reached[name] += 1
+
+    if "report_2" in report[name]:
+        guide_reached[name] += 1
+        max_node[name] = max(max_node[name], report[name]["report"]["egraph_nodes"])
+        reached_after_guide[name] += 1
+        if max_node[name] < max_node["baseline"]:
+            guide_reduced_mem[name] += 1
 
 
 if __name__ == "__main__":
@@ -83,7 +100,7 @@ if __name__ == "__main__":
 
     report_path = usefulness_path / "reports.json"
 
-    if not report_path.exists():
+    if not report_path.exists() or str(sys.argv[3]) == "--force":
         reports = []
         for tuple in tqdm(eval_tuples[:n_samples], desc=f"Evaluating {n_samples} samples"):
             reports.append(check_tuple(tuple))
@@ -97,23 +114,56 @@ if __name__ == "__main__":
     stop_reasons: dict[str, DefaultDict[str, int]] = {
         "baseline": defaultdict(int),
         "middle": defaultdict(int),
+        "middle_as_sketch": defaultdict(int),
         "generated": defaultdict(int),
     }
-    guide_found = {"middle": 0, "generated": 0}
-    reached_after_guide = {"middle": 0, "generated": 0}
-    guide_reduced_mem = {"middle": 0, "generated": 0}
+    guide_reached = defaultdict(int)
+    reached_after_guide = defaultdict(int)
+    guide_reduced_mem = defaultdict(int)
+    goal_reached = defaultdict(int)
 
     for report in reports:
         max_node = {}
 
         parse_guided_report(
-            report, "baseline", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+            report,
+            "baseline",
+            stop_reasons,
+            goal_reached,
+            guide_reached,
+            reached_after_guide,
+            guide_reduced_mem,
+            max_node,
         )
         parse_guided_report(
-            report, "middle", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+            report,
+            "middle",
+            stop_reasons,
+            goal_reached,
+            guide_reached,
+            reached_after_guide,
+            guide_reduced_mem,
+            max_node,
         )
         parse_guided_report(
-            report, "generated", stop_reasons, guide_found, reached_after_guide, guide_reduced_mem, max_node
+            report,
+            "middle_as_sketch",
+            stop_reasons,
+            goal_reached,
+            guide_reached,
+            reached_after_guide,
+            guide_reduced_mem,
+            max_node,
+        )
+        parse_guided_report(
+            report,
+            "generated",
+            stop_reasons,
+            goal_reached,
+            guide_reached,
+            reached_after_guide,
+            guide_reduced_mem,
+            max_node,
         )
 
         max_nodes.append(max_node)
@@ -126,8 +176,11 @@ if __name__ == "__main__":
     with open(usefulness_path / "stop_reasons.json", mode="w", encoding="utf-8") as f:
         json.dump(stop_reasons_dict, f)
 
-    with open(usefulness_path / "guide_found.json", mode="w", encoding="utf-8") as f:
-        json.dump(guide_found, f)
+    with open(usefulness_path / "goal_reached.json", mode="w", encoding="utf-8") as f:
+        json.dump(goal_reached, f)
+
+    with open(usefulness_path / "guide_reached.json", mode="w", encoding="utf-8") as f:
+        json.dump(guide_reached, f)
 
     with open(usefulness_path / "reached_after_guide.json", mode="w", encoding="utf-8") as f:
         json.dump(reached_after_guide, f)
@@ -142,8 +195,10 @@ if __name__ == "__main__":
 
     print("---\nSTOP REASONS")
     print(stop_reasons)
-    print("---\nGUIDES FOUND")
-    print(guide_found)
+    print("---\nGOALS REACHED")
+    print(goal_reached)
+    print("---\nGUIDES REACHED")
+    print(guide_reached)
     print("---\nREACHED AFTER GUIDE")
     print(reached_after_guide)
     print("---\nGUIDE REDUCED MEM")
