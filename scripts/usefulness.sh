00b2721800d0644fe6ea7535fb594d8d852f2609
Nicole Heinimann <heinimann@tu-berlin.de>
2025-10-06 15:13:44 +0000

    updated to new eggshell version and script fix

--- scripts/usefulness.sh
+++ scripts/usefulness.sh
@@ -1,4 +1,2 @@
-uv run usefulness.py 25-09-17-11:52:29 train --force > usefulness-many-train.log
-uv run usefulness.py 25-09-17-11:52:29 eval --force > usefulness-many-eval.log
-uv run usefulness.py 25-09-17-16:34:00 train --force > usefulness-few-train.log
-uv run usefulness.py 25-09-17-16:34:00 eval --force > usefulness-few-eval.log
+uv run usefulness.py 25-10-06-12:54:16 train --force > usefulness-train.log
+uv run usefulness.py 25-10-06-12:54:16 eval --force > usefulness-eval.log
